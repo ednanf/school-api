7372ff26559b2127b9a82f4641f3f9c0d6151f09
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2026-09-03 11:33:05 -0300

    Scaffold class and create the domain layer

--- internal/domain/class.go
+++ internal/domain/class.go
@@ -1,11 +1,30 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
+// ClassRepository defines the contract for database operations (in class_repo.go)
+type ClassRepository interface {
+	Create(ctx context.Context, class *Class) error
+	Delete(ctx context.Context, id int) error
+	GetById(ctx context.Context, id int) (*Student, error)
+	List(ctx context.Context, limit int, offset int) ([]Student, error)
+	Update(ctx context.Context, id int, input PatchClassInput) (*Class, error)
+}
+
+// Class defines the shape of Class struct in the database
 type Class struct {
 	ID        int       `json:"id" db:"id"`
 	Grade     int       `json:"grade" db:"grade" validate:"required,min=1,max=8"`
 	Letter    string    `json:"letter" db:"letter" validate:"required,oneof=A B"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// PatchClassInput defines the JSON payload for inserting one class
+type PatchClassInput struct {
+	Grade  int    `json:"grade" db:"grade" validate:"required,min=1,max=8"`
+	Letter string `json:"letter" db:"letter" validate:"required,oneof=A B"`
 }
