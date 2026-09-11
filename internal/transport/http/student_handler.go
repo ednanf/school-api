66587ab43ecf289035f10e131080381b6de32760
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2026-09-11 10:48:06 -0300

    Add TODO

--- internal/transport/http/student_handler.go
+++ internal/transport/http/student_handler.go
@@ -40,6 +40,8 @@ func (h *StudentHandler) StudentRoutes() chi.Router {
 	return r
 }
 
+// TODO: Make an endpoint to list all students in a specific class `/students/class/{id}`
+
 func (h *StudentHandler) HandleBulkCreate(w http.ResponseWriter, r *http.Request) {
 	// Instantiate a variable to hold the payload according to the DTO (for validation and structure)
 	var input domain.BatchCreateStudentInput
